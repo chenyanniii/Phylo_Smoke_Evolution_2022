a30a18eecd2087d14175102b52107c6f6b6a8cff
chenyanniii <42744112+chenyanniii@users.noreply.github.com>
2022-07-04 16:01:36 -0500

    correct typo and fix links

--- wrap.R
+++ wrap.R
@@ -7,11 +7,32 @@ source("scripts/4_Part0_lib_dat_prep.R")
 ######## calculation for all species
 
 ## phylogenetic signal
-source("scripts/Part1_PhyloSig.R")
+source("scripts/Part1_PhySig.R")
 ## ancestral state reconstruction
-source("scripts/Part2_AncestralState")
+source("scripts/Part2_AncestralState.R")
 
 ######## subtree calculation
-subtree_analysis("Trichocline_spathulata","Liatris_pycnostachya", tree_joint_match, smokelist_joint_match )
+cols = c("blue", "red")
+names(cols) = levels(smoke_responses_joint)
 
-subtree_analysis("Woollsia_pungens", "Leucopogon_capitellatus", tree_joint_match, smokelist_joint_match )
+## Asteraceae Clade
+
+ spp1 = "Trichocline_spathulata"
+ spp2 = "Liatris_pycnostachya"
+
+master.tree =  tree_joint_match
+master.smokelist = smokelist_joint_match
+
+subtree = extract_subtree(spp1, spp2, master.tree)
+
+subtree_analysis("Trichocline_spathulata","Liatris_pycnostachya", tree_joint_match, smokelist_joint_match)
+
+
+
+## Ericaceae Clade
+# spp1 = "Woollsia_pungens"
+# spp2 = "Leucopogon_capitellatus"
+
+# master.tree =  tree_joint_match
+# master.smokelist = smokelist_joint_match
+subtree_analysis("Woollsia_pungens", "Leucopogon_capitellatus", tree_joint_match, smokelist_joint_match)
